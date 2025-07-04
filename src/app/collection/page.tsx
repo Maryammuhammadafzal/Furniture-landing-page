34b701574fd0eb55a8f849ee3d0e8e803d9a5e12
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-05 00:03:02 +0500

    adding animation

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React from "react";
 
 const CollectionPage = () => {
+    
   return (
     <div className="w-full h-auto md:py-30 sm:py-20 py-10 flex flex-col justify-center items-center">
       <h2 className="max-w-4xl font-mono md:text-6xl sm:text-5xl text-4xl text-primary text-center">
@@ -11,11 +12,11 @@ const CollectionPage = () => {
 
       <div className="w-full px-6 py-30 h-auto flex justify-center items-center">
         <div className="relative w-full min-h-[700px] flex flex-col gap-40 justify-center items-center">
-          <div className="w-full h-auto flex justify-evenly gap-8 items-center">
-            <h2 className="max-w-lg leading-16 font-mono md:text-6xl sm:text-5xl text-4xl text-primary ">
+          <div className="slide-card w-full h-auto flex justify-evenly gap-8 items-center">
+            <h2 className="card-heading max-w-lg leading-16 font-mono md:text-6xl sm:text-5xl text-4xl text-primary ">
               Living Room Furniture
             </h2>
-            <div className="flex relative justify-center items-center rounded-full h-[100px] w-[100px]">
+            <div className="flex relative justify-center items-center rounded-full h-[100px] w-[100px] slide-arrow">
               <svg
                 className="absolute inset-0 w-full h-full"
                 preserveAspectRatio="none"
@@ -37,7 +38,7 @@ const CollectionPage = () => {
                 <MoveRightIcon size={42} color="#31554F" />
               </div>
             </div>
-            <div className="image w-[390px] h-[435px] flex justify-center items-center rotate-6">
+            <div className="slider-image w-[390px] h-[435px] flex justify-center items-center rotate-6">
               <Image
                 src="/images/collection-image1.jpg"
                 alt="colleection-images"
@@ -47,8 +48,8 @@ const CollectionPage = () => {
               />
             </div>
           </div>
-          <div className="w-full h-auto flex justify-evenly gap-8 items-center">
-            <div className="image w-[390px] h-[435px] flex justify-center items-center -rotate-6">
+          <div className="slide-card w-full h-auto flex justify-evenly gap-8 items-center">
+            <div className="slider-image w-[390px] h-[435px] flex justify-center items-center -rotate-6">
               <Image
                 src="/images/collection-image2.jpg"
                 alt="colleection-images"
@@ -57,7 +58,7 @@ const CollectionPage = () => {
                 className="w-full h-full object-cover object-center"
               />
             </div>
-            <div className="flex relative rotate-180 justify-center items-center rounded-full h-[100px] w-[100px]">
+            <div className="flex relative rotate-180 justify-center items-center rounded-full h-[100px] w-[100px] slide-arrow">
               <svg
                 className="absolute inset-0 w-full h-full"
                 preserveAspectRatio="none"
@@ -79,15 +80,15 @@ const CollectionPage = () => {
                 <MoveRightIcon size={42} color="#31554F" />
               </div>
             </div>
-            <h2 className="max-w-lg leading-16 font-mono md:text-6xl sm:text-5xl text-4xl text-primary ">
+            <h2 className="card-heading max-w-lg leading-16 font-mono md:text-6xl sm:text-5xl text-4xl text-primary ">
               Bedroom Furniture
             </h2>
           </div>
-          <div className="w-full h-auto flex justify-evenly gap-8 items-center">
-            <h2 className="max-w-lg leading-16 font-mono md:text-6xl sm:text-5xl text-4xl text-primary ">
+          <div className="slide-card w-full h-auto flex justify-evenly gap-8 items-center">
+            <h2 className="card-heading max-w-lg leading-16 font-mono md:text-6xl sm:text-5xl text-4xl text-primary ">
               Dining Room Furniture
             </h2>
-            <div className="flex relative justify-center items-center rounded-full h-[100px] w-[100px]">
+            <div className="flex relative justify-center items-center rounded-full h-[100px] w-[100px] slide-arrow">
               <svg
                 className="absolute inset-0 w-full h-full"
                 preserveAspectRatio="none"
@@ -109,7 +110,7 @@ const CollectionPage = () => {
                 <MoveRightIcon size={42} color="#31554F" />
               </div>
             </div>
-            <div className="image w-[390px] h-[435px] flex justify-center items-center rotate-6">
+            <div className="slider-image w-[390px] h-[435px] flex justify-center items-center rotate-6">
               <Image
                 src="/images/collection-image3.jpg"
                 alt="colleection-images"
@@ -119,8 +120,8 @@ const CollectionPage = () => {
               />
             </div>
           </div>
-          <div className="w-full h-auto flex justify-evenly gap-8 items-center">
-            <div className="image w-[390px] h-[435px] flex justify-center items-center -rotate-6">
+          <div className="slide-card w-full h-auto flex justify-evenly gap-8 items-center">
+            <div className="slider-image w-[390px] h-[435px] flex justify-center items-center -rotate-6">
               <Image
                 src="/images/collection-image4.jpg"
                 alt="colleection-images"
@@ -129,7 +130,7 @@ const CollectionPage = () => {
                 className="w-full h-full object-cover object-center"
               />
             </div>
-            <div className="flex relative rotate-180 justify-center items-center rounded-full h-[100px] w-[100px]">
+            <div className="flex relative rotate-180 justify-center items-center rounded-full h-[100px] w-[100px] slide-arrow">
               <svg
                 className="absolute inset-0 w-full h-full"
                 preserveAspectRatio="none"
@@ -151,15 +152,15 @@ const CollectionPage = () => {
                 <MoveRightIcon size={42} color="#31554F" />
               </div>
             </div>
-            <h2 className="max-w-lg leading-16 font-mono md:text-6xl sm:text-5xl text-4xl text-primary ">
+            <h2 className="card-heading max-w-lg leading-16 font-mono md:text-6xl sm:text-5xl text-4xl text-primary ">
               Outdoor Furniture
             </h2>
           </div>
-          <div className="w-full h-auto flex justify-evenly gap-8 items-center">
-            <h2 className="max-w-lg leading-16 font-mono md:text-6xl sm:text-5xl text-4xl text-primary ">
+          <div className="slide-card w-full h-auto flex justify-evenly gap-8 items-center">
+            <h2 className="card-heading max-w-lg leading-16 font-mono md:text-6xl sm:text-5xl text-4xl text-primary ">
               Office Furniture
             </h2>
-            <div className="flex relative justify-center items-center rounded-full h-[100px] w-[100px]">
+            <div className="flex relative justify-center items-center rounded-full h-[100px] w-[100px] slide-arrow">
               <svg
                 className="absolute inset-0 w-full h-full"
                 preserveAspectRatio="none"
@@ -181,7 +182,7 @@ const CollectionPage = () => {
                 <MoveRightIcon size={42} color="#31554F" />
               </div>
             </div>
-            <div className="image w-[390px] h-[435px] flex justify-center items-center rotate-6">
+            <div className="slider-image w-[390px] h-[435px] flex justify-center items-center rotate-6">
               <Image
                 src="/images/collection-image5.jpg"
                 alt="colleection-images"
@@ -191,8 +192,8 @@ const CollectionPage = () => {
               />
             </div>
           </div>
-          <div className="w-full h-auto flex justify-evenly gap-8 items-center">
-            <div className="image w-[390px] h-[435px] flex justify-center items-center rotate-6">
+          <div className="slide-card w-full h-auto flex justify-evenly gap-8 items-center">
+            <div className="slider-image w-[390px] h-[435px] flex justify-center items-center rotate-6">
               <Image
                 src="/images/collection-image6.jpg"
                 alt="colleection-images"
@@ -201,7 +202,7 @@ const CollectionPage = () => {
                 className="w-full h-full object-cover object-center"
               />
             </div>
-            <div className="flex relative justify-center rotate-180 items-center rounded-full h-[100px] w-[100px]">
+            <div className="flex relative justify-center rotate-180 items-center rounded-full h-[100px] w-[100px] slide-arrow">
               <svg
                 className="absolute inset-0 w-full h-full"
                 preserveAspectRatio="none"
@@ -223,7 +224,7 @@ const CollectionPage = () => {
                 <MoveRightIcon size={42} color="#31554F" />
               </div>
             </div>
-            <h2 className="max-w-lg leading-16 font-mono md:text-6xl sm:text-5xl text-4xl text-primary ">
+            <h2 className="card-heading max-w-lg leading-16 font-mono md:text-6xl sm:text-5xl text-4xl text-primary ">
               Custom Furniture
             </h2>
           </div>
