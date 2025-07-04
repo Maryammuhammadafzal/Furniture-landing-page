a7c799740aadbbed54f74d09707ebb38a7842660
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-04 23:05:17 +0500

    creating collection section

---
+++ src/app/collection/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CollectionPage = () => {
+  return (
+    <div className="w-full h-auto md:py-30 sm:py-20 py-10 flex justify-center items-center">
+      <h2 className="max-w-4xl font-mono md:text-6xl sm:text-5xl text-4xl text-primary text-center">
+        Discover Our Unique Furniture Collections
+      </h2>
+
+      <div className="w-full px-6 h-auto flex justify-center items-center">
+        <div className="relative"></div>
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import CollectionPage from "./collection/page";
 
 export default function Home() {
   return (
-   <div className="w-full h-auto overflow-hidden"></div>
+   <div className="w-full h-auto overflow-hidden">
+    <CollectionPage/>
+   </div>
   );
 }
